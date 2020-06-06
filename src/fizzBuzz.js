676a173804b95f5cc0094fe100835ae5111ca7e2
Sayem995 <sayem.bokhari.1@gmail.com>
2020-06-06 18:38:44 +0500

    test passing

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -4,11 +4,13 @@ class FizzBuzz {
     }
 
     play(number) {
-        if (this._isDivisibleBy(5, number)) {
-            return 'Buzz';
+        if (this._isDivisibleBy(15, number)) {
+            return 'FizzBuzz';
         }
-        else if (this._isDivisibleBy(3, number)) {
-            return 'Fizz';
+        else if (this._isDivisibleBy(5, number)) {
+            return 'Buzz';
+        } else if (this._isDivisibleBy(3, number)) {
+            return "Fizz";
         } else {
             return number;
         }
